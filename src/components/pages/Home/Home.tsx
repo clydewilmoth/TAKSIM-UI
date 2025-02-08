edb1a2889ad05367f150712e87b5ff4460d26a4e
selim <selimyag.eser@gmail.com>
2025-02-08 17:22:41 +0100

    First styles done

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 function Home() {
   return (
     <div id="home" className="pages">
       <div className="block">
-        <h1>Willkommen bei der Bäckerei Taksim im Herzen von Mannheim</h1>
+        <h1>
+          Willkommen bei der Bäckerei Taksim <br />
+          im Herzen von Mannheim
+        </h1>
+        <hr />
         <p>
-          Hier trifft Handwerkskunst auf Tradition, um dir das Beste aus der
-          Welt der Süßspeisen zu bieten. Unsere Produkte entstehen mit viel
-          Liebe zum Detail und sorgfältig ausgewählten Zutaten – ganz so, wie es
-          die Tradition verlangt.
+          Bei Uns trifft Handwerkskunst auf Tradition, um dir das Beste aus der
+          Welt der Backwaren zu bieten. Unsere Produkte entstehen mit viel Liebe
+          zum Detail und sorgfältig ausgewählten Zutaten.
         </p>
         <p>
           Entdecke unsere große Auswahl an Baklava, Kuchen und anderen
@@ -18,11 +22,11 @@ function Home() {
           unverwechselbarem Geschmack.
         </p>
         <h2>Hier finden Sie uns:</h2>
+        <hr />
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d323.95812674011904!2d8.465556493028995!3d49.49083447554156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4797cc24dc58cd8f%3A0x8477dbef2ed4c00c!2sTaksim%20Baklavaci!5e0!3m2!1sde!2sde!4v1739028295387!5m2!1sde!2sde"
-          width="700"
-          height="350"
-          style={{ border: 0 }}
+          width="1200"
+          height="800"
           allowFullScreen={false}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
