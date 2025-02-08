6d8016b9685ef0769da2294303dd24307486ce8e
selim <selimyag.eser@gmail.com>
2025-02-08 03:36:22 +0100

    Compressed Nav component

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -2,55 +2,37 @@ import { Link, useLocation } from "react-router-dom";
 import "./styles.css";
 
 function Nav() {
-  let location = useLocation();
-  let classNames = ["nav-current", "nav-element", "nav-element", "nav-element"];
-  switch (location.pathname) {
-    case "/":
-      classNames[1] = "nav-element";
-      classNames[2] = "nav-element";
-      classNames[3] = "nav-element";
-      classNames[0] = "nav-current";
-      break;
-    case "/about":
-      classNames[0] = "nav-element";
-      classNames[2] = "nav-element";
-      classNames[3] = "nav-element";
-      classNames[1] = "nav-current";
-      break;
-    case "/order":
-      classNames[0] = "nav-element";
-      classNames[1] = "nav-element";
-      classNames[3] = "nav-element";
-      classNames[2] = "nav-current";
-      break;
-    case "/contact":
-      classNames[0] = "nav-element";
-      classNames[1] = "nav-element";
-      classNames[2] = "nav-element";
-      classNames[3] = "nav-current";
-      break;
-  }
+  const path = useLocation().pathname;
 
   return (
     <nav id="nav-header">
       <span>
-        <Link to="/" className={classNames[0]}>
-          Home
+        <Link to="/" className={path === "/" ? "nav-current" : "nav-element"}>
+          Startseite
         </Link>
       </span>
       <span>
-        <Link to="/about" className={classNames[1]}>
-          About
+        <Link
+          to="/about"
+          className={path === "/about" ? "nav-current" : "nav-element"}
+        >
+          Über Uns
         </Link>
       </span>
       <span>
-        <Link to="/order" className={classNames[2]}>
-          Order
+        <Link
+          to="/order"
+          className={path === "/order" ? "nav-current" : "nav-element"}
+        >
+          Bestellen
         </Link>
       </span>
       <span>
-        <Link to="/contact" className={classNames[3]}>
-          Contact
+        <Link
+          to="/contact"
+          className={path === "/contact" ? "nav-current" : "nav-element"}
+        >
+          Kontakt
         </Link>
       </span>
     </nav>
