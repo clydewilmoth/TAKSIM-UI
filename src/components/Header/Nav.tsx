15cf58c9db132a17caa282410dab35d1bed53b27
selim <selimyag.eser@gmail.com>
2025-02-08 02:45:04 +0100

    First header design

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,20 +1,58 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import "./styles.css";
 
 function Nav() {
+  let location = useLocation();
+  let classNames = ["nav-current", "nav-element", "nav-element", "nav-element"];
+  switch (location.pathname) {
+    case "/":
+      classNames[1] = "nav-element";
+      classNames[2] = "nav-element";
+      classNames[3] = "nav-element";
+      classNames[0] = "nav-current";
+      break;
+    case "/about":
+      classNames[0] = "nav-element";
+      classNames[2] = "nav-element";
+      classNames[3] = "nav-element";
+      classNames[1] = "nav-current";
+      break;
+    case "/order":
+      classNames[0] = "nav-element";
+      classNames[1] = "nav-element";
+      classNames[3] = "nav-element";
+      classNames[2] = "nav-current";
+      break;
+    case "/contact":
+      classNames[0] = "nav-element";
+      classNames[1] = "nav-element";
+      classNames[2] = "nav-element";
+      classNames[3] = "nav-current";
+      break;
+  }
+
   return (
-    <nav>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/about">About</Link>
-      </p>
-      <p>
-        <Link to="/order">Order</Link>
-      </p>
-      <p>
-        <Link to="/contact">Contact</Link>
-      </p>
+    <nav id="nav-header">
+      <span>
+        <Link to="/" className={classNames[0]}>
+          Home
+        </Link>
+      </span>
+      <span>
+        <Link to="/about" className={classNames[1]}>
+          About
+        </Link>
+      </span>
+      <span>
+        <Link to="/order" className={classNames[2]}>
+          Order
+        </Link>
+      </span>
+      <span>
+        <Link to="/contact" className={classNames[3]}>
+          Contact
+        </Link>
+      </span>
     </nav>
   );
 }
