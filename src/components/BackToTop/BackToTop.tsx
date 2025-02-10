1b3fea70a695cc5c0c7d27ed3c7844b19e8b140a
Selim Eser <2211482@stud.hs-mannheim.de>
2025-02-10 20:26:56 +0100

    Condition to show BackToTop added

--- src/components/BackToTop/BackToTop.tsx
+++ src/components/BackToTop/BackToTop.tsx
@@ -1,18 +1,39 @@
+import { useEffect, useState } from "react";
 import ArrowUp from "../../assets/arrowup.svg";
 import "./styles.css";
 
 function BackToTop() {
+  const [hidden, setHidden] = useState(true);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        setHidden(false);
+      } else {
+        setHidden(true);
+      }
+    });
+
+    return () =>
+      window.removeEventListener("scroll", () => {
+        if (window.scrollY > 200) {
+          setHidden(false);
+        } else {
+          setHidden(true);
+        }
+      });
+  }, []);
+
   return (
     <img
       src={ArrowUp}
       alt="Pfeil nach oben"
       id="back-to-top"
-      onClick={() => clickHandler()}
+      onClick={() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }}
+      hidden={hidden}
     />
   );
 }
 
-function clickHandler(): void {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-}
 export default BackToTop;
