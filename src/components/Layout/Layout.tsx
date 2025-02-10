3aa1bc721a04d0e46525c9a1567569e5c89f79e1
Selim Eser <2211482@stud.hs-mannheim.de>
2025-02-10 19:17:06 +0100

    Added arrow up image with style id

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import "./styles.css";
+import ArrowUp from "../../assets/arrowup.svg";
 
 function Layout() {
   return (
@@ -10,6 +11,7 @@ function Layout() {
       <div id="content">
         <Outlet />
       </div>
+      <img src={ArrowUp} alt="Pfeil nach oben" id="back-to-top" />
       <Footer />
     </div>
   );
