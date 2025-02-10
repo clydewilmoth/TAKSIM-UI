18c4a8c82670475ae75380a696d9d3af539e3538
Selim Eser <2211482@stud.hs-mannheim.de>
2025-02-10 19:46:24 +0100

    Added onClick Handler

--- src/components/BackToTop/BackToTop.tsx
+++ src/components/BackToTop/BackToTop.tsx
@@ -2,7 +2,17 @@ import ArrowUp from "../../assets/arrowup.svg";
 import "./styles.css";
 
 function BackToTop() {
-  return <img src={ArrowUp} alt="Pfeil nach oben" id="back-to-top" />;
+  return (
+    <img
+      src={ArrowUp}
+      alt="Pfeil nach oben"
+      id="back-to-top"
+      onClick={() => clickHandler()}
+    />
+  );
 }
 
+function clickHandler(): void {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
 export default BackToTop;
