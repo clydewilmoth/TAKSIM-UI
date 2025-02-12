3a4cf0e2a0ad1a6986b3b8c25f5c0597866078bf
selim <selimyag.eser@gmail.com>
2025-02-13 00:16:04 +0100

    Added opening times

--- src/components/pages/Home/OpeningTimes.tsx
+++ src/components/pages/Home/OpeningTimes.tsx
@@ -1,5 +1,22 @@
 function OpeningTimes() {
-  return <div id="opening-times">---</div>;
+  return (
+    <div id="opening-times">
+      <span className="day">Montag</span>
+      <span className="time">06:00 - 22:00</span>
+      <span className="day">Dienstag</span>
+      <span className="time">06:00 - 22:00</span>
+      <span className="day">Mittwoch</span>
+      <span className="time">06:00 - 22:00</span>
+      <span className="day">Donnerstag</span>
+      <span className="time">06:00 - 22:00</span>
+      <span className="day">Freitag</span>
+      <span className="time">06:00 - 23:00</span>
+      <span className="day">Samstag</span>
+      <span className="time">06:00 - 23:00</span>
+      <span className="day">Sonntag</span>
+      <span className="time">06:00 - 23:00</span>
+    </div>
+  );
 }
 
 export default OpeningTimes;
